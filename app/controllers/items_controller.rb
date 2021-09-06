7253b82d69831c10904275c69a41f9ff34e5ab95
Travis Courtney <travis@angry-fly.com>
2021-09-06 00:20:55 -0400

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(item_params)
+    if item
+      render json: item, status: :created
+    end
+  end
+
+  private 
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def record_not_found_response( exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
